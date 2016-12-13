01a08a884ce76656348f919d7e9787f3a21fec34
jaroen007 <jaroeneefting@hotmail.com>
2016-12-13 07:21:49 +0100

    initial index

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="nl">
+<head>
+  <meta charset="utf-8"?>
+  <title>Title</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	content
+</body>
+</html>
