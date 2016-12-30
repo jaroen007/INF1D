2ac4955c2cbcc45972dec3838b552b1078637f62
nickschalen <nickschalen@gmail.com>
2016-12-30 19:23:24 +0100

    taalswitch deels

--- includes/topbar.php
+++ includes/topbar.php
@@ -1,3 +1,4 @@
+
 <div class="topbarcontainer">
 	<div class="topbar">
 
@@ -29,6 +30,17 @@
 		<ul>
 		
 		<div class="clear"></div>
-		
+		<div id="languages">
+                <form method="post" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
+                    <label class="languagehover">
+                        <input type="radio" name="language" value="dutch" onchange="post(this.value);" <?php if($language == "dutch"){ echo "checked=\"checked\"";} ?> />
+                        <img src="images/dutch.png" alt="dutch" />
+                    </label>
+                    <label class="languagehover">
+                        <input type="radio" name="language" value="english" onchange="post(this.value);" <?php if($language == "english"){ echo "checked=\"checked\"";} ?> />	
+                        <img src="images/english.png" alt="english" />
+                    </label>
+                </form>
+            </div>
 	</div>
 </div>
