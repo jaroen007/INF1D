2b3bf3024e86592a72332cc52f19366182a20284
Zanril95 <jeroen-wind@live.nl>
2017-01-04 19:27:50 +0100

    Upload script
    Met eigen test, anderen graag ook testen

---
+++ UploadForm.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Upload Form</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<form method="post" action="UploadFile.php" enctype="multipart/form-data">
+			<p><input type="file" name="fileToUpload" id="fileToUpload" /></p>
+			<p><input type="submit" name="submit" /></p>
+		</form>
+	</body>
+</html>
