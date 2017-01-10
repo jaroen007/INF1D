d5a2466ca2690e645e4ed91313a3cd5b997b541c
Corneetje <corneevanblerk@gmail.com>
2017-01-10 12:20:57 +0100

    123

--- page/corneeslegacy.php
+++
@@ -1,9 +0,0 @@
-<div class="contentcontainer">
-    <div class="content">
-		<?php 
-            $core = new Core;
-            $core->getPortfolio($_SESSION['id']);
-        ?>
-
-    </div>
-</div>
