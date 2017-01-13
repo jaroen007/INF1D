5380029d942954c7409be5073fce19effe613bea
jaroen007 <jaroeneefting@hotmail.com>
2017-01-13 14:11:17 +0100

    DELETE

--- includes/language.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    if (!isset($_SESSION['language'])) {
-        $language = "dutch";
-    } else {
-        $_SESSION["language"] = $_GET['language'];
-        $language = $_SESSION['language'];
-    }
-?>
--- page/bewerkPortfolio.php
+++
@@ -1,9 +0,0 @@
-                <?php
-                $core = new Core;
-                $core->getPortfolio($_SESSION['id']);
-                ?>
-
-                <div class="clear"></div>
-        
-    </div>
-</div>
